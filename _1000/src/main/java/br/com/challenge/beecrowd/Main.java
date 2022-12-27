3a137121d129216813562dd83115dc59ba969013
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2022-12-27 10:59:25 -0300

    add challenge 1000

---
+++ _1000/src/main/java/br/com/challenge/beecrowd/Main.java
@@ -0,0 +1,9 @@
+package br.com.challenge.beecrowd;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
